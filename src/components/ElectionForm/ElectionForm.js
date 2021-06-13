d5cfc5570a6d26070626df438c9ee05e3ede8953
[Abid] <[abid.hassan0801@gmail.com]>
2021-06-13 20:07:00 +0600

    Radio buttion fixed

--- src/components/ElectionForm/ElectionForm.js
+++ src/components/ElectionForm/ElectionForm.js
@@ -12,11 +12,11 @@ export default function ElectionForm(props) {
                         <p></p>
                         <p>First Choice</p>
                         <form>
-                        <input type="radio" id={props.chairFirstCandidate} name={props.name} value={props.firstCandidate}></input>
-                        <label for={props.firstCandidate}>{props.firstCandidate}</label><br></br>   
+                        <input type="radio" id="{props.chairFirstCandidate}" name="{props.name}" value="{props.firstCandidate}"></input>
+                        <label for="{props.firstCandidate}">{props.firstCandidate}</label><br></br>   
 
-                        <input type="radio" id={props.ChairSecondCandidate} name={props.name} value={props.secondCandidate}></input>
-                        <label for={props.secondCandidate}>{props.secondCandidate}</label><br></br>    
+                        <input type="radio" id="{props.ChairSecondCandidate}" name="{props.name}" value="{props.secondCandidate}"></input>
+                        <label for="{props.secondCandidate}">{props.secondCandidate}</label><br></br>    
                         
                        </form>
 
@@ -24,10 +24,10 @@ export default function ElectionForm(props) {
 
                        <form>
                           
-                        <input type="radio" id={props.firstCandidate} name={props.name} value={props.firstCandidate}></input>
-                        <label for={props.firstCandidate}>{props.firstCandidate}</label><br></br>  
-                        <input type="radio" id={props.secondCandidate} name={props.name} value={props.secondCandidate}></input>
-                        <label for={props.secondCandidate}>{props.secondCandidate}</label><br></br>
+                        <input type="radio" id="{props.firstCandidate}" name="{props.name}" value="{props.firstCandidate}"></input>
+                        <label for="{props.firstCandidate}">{props.firstCandidate}</label><br></br>  
+                        <input type="radio" id="{props.secondCandidate}" name="{props.name}" value="{props.secondCandidate}"></input>
+                        <label for="{props.secondCandidate}">{props.secondCandidate}</label><br></br>
                        </form>
                         </div>
 
@@ -37,39 +37,37 @@ export default function ElectionForm(props) {
                  <p></p>
                 <p>First Choice</p>
                 <form>            
-                <input type="radio" id={props.firstCandidate} name={props.name} value={props.firstCandidate}></input>
-                <label for={props.firstCandidate}>{props.firstCandidate}</label><br></br> 
+                <input type="radio" id="{props.firstCandidate}" name="{props.name}" value="{props.firstCandidate}"></input>
+                <label for="{props.firstCandidate}">{props.firstCandidate}</label><br></br> 
                
-                <input type="radio" id={props.secondCandidate} name={props.name} value={props.secondCandidate}></input>
-                <label for={props.secondCandidate}>{props.secondCandidate}</label><br></br>    
+                <input type="radio" id="{props.secondCandidate}" name="{props.name}" value="{props.secondCandidate}"></input>
+                <label for="{props.secondCandidate}">{props.secondCandidate}</label><br></br>    
                    
-                <input type="radio" id={props.thirdCandidate} name={props.name} value={props.thirdCandidate}></input>
-                <label for={props.thirdCandidate}>{props.thirdCandidate}</label><br></br> 
+                <input type="radio" id="{props.thirdCandidate}" name="{props.name}" value="{props.thirdCandidate}"></input>
+                <label for="{props.thirdCandidate}">{props.thirdCandidate}</label><br></br> 
                 </form>
                  <p>Second Choice</p>
-                 <form>
-              
-                <input type="radio" id={props.firstCandidate} name={props.name} value={props.firstCandidate}></input>
-                <label for={props.firstCandidate}>{props.firstCandidate}</label><br></br>    
-                  
-                <input type="radio" id={props.secondCandidate} name={props.name} value={props.secondCandidate}></input>
-                <label for={props.secondCandidate}>{props.secondCandidate}</label><br></br>  
-             
-                <input type="radio" id={props.thirdCandidate} name={props.name} value={props.thirdCandidate}></input>
-                <label for={props.thirdCandidate}>{props.thirdCandidate}</label><br></br>   
+                 <form>            
+                <input type="radio" id="{props.firstCandidate}" name="{props.name}" value="{props.firstCandidate}"></input>
+                <label for="{props.firstCandidate}">{props.firstCandidate}</label><br></br> 
+               
+                <input type="radio" id="{props.secondCandidate}" name="{props.name}" value="{props.secondCandidate}"></input>
+                <label for="{props.secondCandidate}">{props.secondCandidate}</label><br></br>    
+                   
+                <input type="radio" id="{props.thirdCandidate}" name="{props.name}" value="{props.thirdCandidate}"></input>
+                <label for="{props.thirdCandidate}">{props.thirdCandidate}</label><br></br> 
                 </form>
                 <p>Third Choice</p>
-                 <form>
-                    
-                <input type="radio" id={props.firstCandidate} name={props.name} value={props.firstCandidate}></input>
-                <label for={props.firstCandidate}>{props.firstCandidate}</label><br></br>
+                <form>            
+                <input type="radio" id="{props.firstCandidate}" name="{props.name}" value="{props.firstCandidate}"></input>
+                <label for="{props.firstCandidate}">{props.firstCandidate}</label><br></br> 
                
-                <input type="radio" id={props.secondCandidate}name={props.name} value={props.secondCandidate}></input>
-                <label for={props.secondCandidate}>{props.secondCandidate}</label><br></br>    
-     
-                <input type="radio" id={props.thirdCandidate} name={props.name} value={props.thirdCandidate}></input>
-                <label for={props.thirdCandidate}>{props.thirdCandidate}</label><br></br>   
-                </form>  
+                <input type="radio" id="{props.secondCandidate}" name="{props.name}" value="{props.secondCandidate}"></input>
+                <label for="{props.secondCandidate}">{props.secondCandidate}</label><br></br>    
+                   
+                <input type="radio" id="{props.thirdCandidate}" name="{props.name}" value="{props.thirdCandidate}"></input>
+                <label for="{props.thirdCandidate}">{props.thirdCandidate}</label><br></br> 
+                </form>
                 </div>
                 }
 
@@ -77,40 +75,37 @@ export default function ElectionForm(props) {
                  {props.position==="treasurer" && <div>
                  <p></p>
                 <p>First Choice</p>
-                <form>
-     
-                <input type="radio" id={props.firstCandidate} name={props.name} value={props.firstCandidate}></input>
-                <label for={props.firstCandidate}>{props.firstCandidate}</label><br></br>    
-   
-                <input type="radio" id={props.secondCandidate} name={props.name} value={props.secondCandidate}></input>
-                <label for={props.secondCandidate}>{props.secondCandidate}</label><br></br>   
+                <form>            
+                <input type="radio" id="{props.firstCandidate}" name="{props.name}" value="{props.firstCandidate}"></input>
+                <label for="{props.firstCandidate}">{props.firstCandidate}</label><br></br> 
+               
+                <input type="radio" id="{props.secondCandidate}" name="{props.name}" value="{props.secondCandidate}"></input>
+                <label for="{props.secondCandidate}">{props.secondCandidate}</label><br></br>    
                    
-                <input type="radio" id={props.thirdCandidate} name={props.name} value={props.thirdCandidate}></input>
-                <label for={props.thirdCandidate}>{props.thirdCandidate}</label><br></br> 
+                <input type="radio" id="{props.thirdCandidate}" name="{props.name}" value="{props.thirdCandidate}"></input>
+                <label for="{props.thirdCandidate}">{props.thirdCandidate}</label><br></br> 
                 </form>
                  <p>Second Choice</p>
-                 <form>
-                
-                <input type="radio" id={props.firstCandidate} name={props.name} value={props.firstCandidate}></input>
-                <label for={props.firstCandidate}>{props.firstCandidate}</label><br></br>    
-                  
-                <input type="radio" id={props.secondCandidate} name={props.name} value={props.secondCandidate}></input>
-                <label for={props.secondCandidate}>{props.secondCandidate}</label><br></br>  
+                 <form>            
+                <input type="radio" id="{props.firstCandidate}" name="{props.name}" value="{props.firstCandidate}"></input>
+                <label for="{props.firstCandidate}">{props.firstCandidate}</label><br></br> 
+               
+                <input type="radio" id="{props.secondCandidate}" name="{props.name}" value="{props.secondCandidate}"></input>
+                <label for="{props.secondCandidate}">{props.secondCandidate}</label><br></br>    
                    
-                <input type="radio" id={props.thirdCandidate} name={props.name} value={props.thirdCandidate}></input>
-                <label for={props.thirdCandidate}>{props.thirdCandidate}</label><br></br> 
+                <input type="radio" id="{props.thirdCandidate}" name="{props.name}" value="{props.thirdCandidate}"></input>
+                <label for="{props.thirdCandidate}">{props.thirdCandidate}</label><br></br> 
                 </form>
                 <p>Third Choice</p>
-                 <form>
-                
-                <input type="radio" id={props.firstCandidate} name={props.name} value={props.firstCandidate}></input>
-                <label for={props.firstCandidate}>{props.firstCandidate}</label><br></br>    
-                
-                <input type="radio" id={props.secondCandidate}name={props.name} value={props.secondCandidate}></input>
-                <label for={props.secondCandidate}>{props.secondCandidate}</label><br></br>    
-                
-                <input type="radio" id={props.thirdCandidate} name={props.name} value={props.thirdCandidate}></input>
-                <label for={props.thirdCandidate}>{props.thirdCandidate}</label><br></br>    
+                <form>            
+                <input type="radio" id="{props.firstCandidate}" name="{props.name}" value="{props.firstCandidate}"></input>
+                <label for="{props.firstCandidate}">{props.firstCandidate}</label><br></br> 
+               
+                <input type="radio" id="{props.secondCandidate}" name="{props.name}" value="{props.secondCandidate}"></input>
+                <label for="{props.secondCandidate}">{props.secondCandidate}</label><br></br>    
+                   
+                <input type="radio" id="{props.thirdCandidate}" name="{props.name}" value="{props.thirdCandidate}"></input>
+                <label for="{props.thirdCandidate}">{props.thirdCandidate}</label><br></br> 
                 </form>  
                 </div>
                 }
@@ -118,62 +113,43 @@ export default function ElectionForm(props) {
                 {props.position==="secretary" && <div>
                  <p></p>
                 <p>First Choice</p>
-                <form>
+                <form>            
+                <input type="radio" id="{props.firstCandidate}" name="{props.name}" value="{props.firstCandidate}"></input>
+                <label for="{props.firstCandidate}">{props.firstCandidate}</label><br></br> 
+               
+                <input type="radio" id="{props.secondCandidate}" name="{props.name}" value="{props.secondCandidate}"></input>
+                <label for="{props.secondCandidate}">{props.secondCandidate}</label><br></br>    
                    
-                <input type="radio" id={props.firstCandidate} name={props.name} value={props.firstCandidate}></input>
-                <label for={props.firstCandidate}>{props.firstCandidate}</label><br></br>
-                   
-                <input type="radio" id={props.secondCandidate} name={props.name} value={props.secondCandidate}></input>
-                <label for={props.secondCandidate}>{props.secondCandidate}</label><br></br> 
-                  
-                <input type="radio" id={props.thirdCandidate} name={props.name} value={props.thirdCandidate}></input>
-                <label for={props.thirdCandidate}>{props.thirdCandidate}</label><br></br>  
+                <input type="radio" id="{props.thirdCandidate}" name="{props.name}" value="{props.thirdCandidate}"></input>
+                <label for="{props.thirdCandidate}">{props.thirdCandidate}</label><br></br> 
                 </form>
                  <p>Second Choice</p>
-                 <form>
-                
-                <input type="radio" id={props.firstCandidate} name={props.name} value={props.firstCandidate}></input>
-                <label for={props.firstCandidate}>{props.firstCandidate}</label><br></br>   
-   
-                <input type="radio" id={props.secondCandidate} name={props.name} value={props.secondCandidate}></input>
-                <label for={props.secondCandidate}>{props.secondCandidate}</label><br></br>   
-                
-                <input type="radio" id={props.thirdCandidate} name={props.name} value={props.thirdCandidate}></input>
-                <label for={props.thirdCandidate}>{props.thirdCandidate}</label><br></br>  
+                 <form>            
+                <input type="radio" id="{props.firstCandidate}" name="{props.name}" value="{props.firstCandidate}"></input>
+                <label for="{props.firstCandidate}">{props.firstCandidate}</label><br></br> 
+               
+                <input type="radio" id="{props.secondCandidate}" name="{props.name}" value="{props.secondCandidate}"></input>
+                <label for="{props.secondCandidate}">{props.secondCandidate}</label><br></br>    
+                   
+                <input type="radio" id="{props.thirdCandidate}" name="{props.name}" value="{props.thirdCandidate}"></input>
+                <label for="{props.thirdCandidate}">{props.thirdCandidate}</label><br></br> 
                 </form>
                 <p>Third Choice</p>
 
-                 <form>
-       
-                <input type="radio" id={props.firstCandidate} name={props.name} value={props.firstCandidate}></input>
-                <label for={props.firstCandidate}>{props.firstCandidate}</label><br></br>    
-                  
-                <input type="radio" id={props.secondCandidate} name={props.name} value={props.secondCandidate}></input>
-                <label for={props.secondCandidate}>{props.secondCandidate}</label><br></br>  
- 
-                <input type="radio" id={props.thirdCandidate} name={props.name} value={props.thirdCandidate}></input>
-                <label for={props.thirdCandidate}>{props.thirdCandidate}</label><br></br>    
+                <form>            
+                <input type="radio" id="{props.firstCandidate}" name="{props.name}" value="{props.firstCandidate}"></input>
+                <label for="{props.firstCandidate}">{props.firstCandidate}</label><br></br> 
+               
+                <input type="radio" id="{props.secondCandidate}" name="{props.name}" value="{props.secondCandidate}"></input>
+                <label for="{props.secondCandidate}">{props.secondCandidate}</label><br></br>    
+                   
+                <input type="radio" id="{props.thirdCandidate}" name="{props.name}" value="{props.thirdCandidate}"></input>
+                <label for="{props.thirdCandidate}">{props.thirdCandidate}</label><br></br> 
                 </form>
                 
-
-                 
                 </div>
                 }
-
-                  
-
-
-                
-
-
-
-      
-
-
             </div>
-        
-            
-
         </div>
     )
 }
